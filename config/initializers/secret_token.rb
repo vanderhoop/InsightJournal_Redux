38ab3a0eccc49fe0e0d8e6145f5c6fdeb8347958
Travis Vander Hoop <vanderhoop@me.com>
2013-12-12 14:51:23 -0500

    initialized rails 3.2.14 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InsightJournalRedux::Application.config.secret_token = 'cce57532e7c529b560bac36df34b2d620c2638700e54222bac415495aa394aeb934d96fe93d2c95baed39f713f5daee3eef5f3058e432792ff3210f982a0cf20'
