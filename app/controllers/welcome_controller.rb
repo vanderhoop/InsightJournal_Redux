8aef12b5ec9bb77924291026303652f1ebd4e1a9
Travis Vander Hoop <vanderhoop@me.com>
2013-12-16 17:11:24 -0500

    added select options for an entries_by_time_of_day filter on the welcome#insights page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,8 @@ class WelcomeController < ApplicationController
     @overall_mood_score = avg_mood_score(current_user.entries)
     @avg_word_count = avg_word_count(current_user.entries)
     @most_common_tense = get_tense_mode(current_user.entries)
-    binding.pry
+    # here is how I will cull entries in the db
+    current_user.entries.where(tense_orientation:"present", user_mood_input: 6)
   end
 
 end
