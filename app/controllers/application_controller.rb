0acc0bba3c8caa6259f3faa299b12a60b4464c93
Travis Vander Hoop <vanderhoop@me.com>
2013-12-16 21:55:09 -0500

    refactored the average_user_mood_input, average_word_count into the return_insights_hash function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,18 +18,18 @@ class ApplicationController < ActionController::Base
     tenses.mode
   end
 
-  def avg_mood_score(entries)
-    avg_mood = entries.average("user_mood_input")
-  end
-
-  def avg_word_count(entries)
-    avg_word_count = entries.average("word_count")
-  end
-
   def get_tense_mode(entries)
    # retrieves tense_orientation values and removes any nil values
    tense_orientations = entries.pluck(:tense_orientation).compact
    tense_mode = tense_orientations.mode
+  end
+
+  def return_insights_hash(entries)
+    insights_hash = {}
+    insights_hash[:avg_mood] = entries.average("user_mood_input")
+    insights_hash[:avg_word_count] = entries.average("word_count")
+    insights_hash[:tense_mode] = get_tense_mode(entries)
+    insights_hash
   end
 
   def filter_entries_by_time_written(array_of_entries, time_of_day)
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,9 +7,10 @@ class WelcomeController < ApplicationController
   end
 
   def insights
-    @overall_mood_score = avg_mood_score(current_user.entries)
-    @avg_word_count = avg_word_count(current_user.entries)
-    @most_common_tense = get_tense_mode(current_user.entries)
+    # @overall_mood_score = avg_mood_score(current_user.entries)
+    # @avg_word_count = avg_word_count(current_user.entries)
+    # @most_common_tense = get_tense_mode(current_user.entries)
+    @insights = return_insights_hash(current_user.entries)
     # here is how I will cull entries in the db
     # current_user.entries.where(tense_orientation:"present", user_mood_input: 6)
   end
@@ -17,6 +18,9 @@ class WelcomeController < ApplicationController
   def new_insights
     time_of_day_desired = params["timeOfDay"]
     @time_filtered_entries = filter_entries_by_time_written(current_user.entries, time_of_day_desired)
+    @new_
+    @new_insights = {}
+
     respond_to do |format|
       format.json do
         render json: @time_filtered_entries
