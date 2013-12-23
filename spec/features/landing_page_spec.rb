dc67eb0e8c9b5643ad60792f18a4d133a96a8d3d
Travis Vander Hoop <vanderhoop@me.com>
2013-12-23 10:49:02 -0600

    deleted duplicate Capybara tests resulting from reorganization process

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -9,65 +9,40 @@ describe 'The Landing Page', :js => true do
     visit '/'
   end
 
-    context "when users aren't logged in" do
+  context "when users aren't logged in" do
 
-      it "links to a sign up page" do
-        page.has_link?("Sign up")
+    it "links to a sign up page" do
+      page.has_link?("Sign up")
+    end
+
+    it "doesn't display the logout button" do
+      expect(page).to_not have_css("input[value='Log out']")
+    end
+
+    it "doesn't display the Insights link" do
+      page.has_no_link? "Insights"
+    end
+
+    it "doesn't display the Write link" do
+      page.has_no_link?("Write")
+    end
+
+    it "displays the login form" do
+      expect(page).to have_css("form[action='/users/sign_in']")
+    end
+
+    describe 'the login form' do
+
+      it "has an 'Email' text input" do
+        page.has_field?("Email")
       end
 
-      it "doesn't display the logout button" do
-        expect(page).to_not have_css("input[value='Log out']")
+      it "has a 'Password' text input" do
+        page.has_field?("Password")
       end
 
-      it "doesn't display the Insights link" do
-        page.has_no_link? "Insights"
-      end
+    end # describe - the login form
 
-      it "doesn't display the Write link" do
-        page.has_no_link?("Write")
-      end
-
-      it "displays the login form" do
-        expect(page).to have_css("form[action='/users/sign_in']")
-      end
-
-      describe 'the login form' do
-
-        it "has an 'Email' text input" do
-          page.has_field?("Email")
-        end
-
-        it "has a 'Password' text input" do
-          page.has_field?("Password")
-        end
-
-      end # describe - the login form
-
-    end # context - when users aren't logged in
-
-    context "when users are logged in" do
-      before(:each) do
-        @user = User.new
-        @user.name = "Bob Joe"
-        @user.email = Faker::Internet.email
-        @user.password = "batman11"
-        @user.password_confirmation = "batman11"
-        @user.save
-        login_as(@user, :scope => :user)
-      end
-
-      it "should link to the insights page" do
-        page.has_link?("Insights")
-      end
-
-      it "should link to the new entry action" do
-        page.has_link?("Write")
-      end
-
-      it "should display a log out button" do
-        page.has_link?("Log out")
-      end
-
-    end # context - when users are logged in
+  end # context - when users aren't logged in
 
 end # describe - The Landing Page
--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -7,15 +7,14 @@ Warden.test_mode!
 feature 'Logging In', :js => true do
 
   context "when users aren't logged in" do
+    before(:each) do
+      Warden.test_reset!
+      visit '/'
+    end
 
-      before(:each) do
-        Warden.test_reset!
-        visit '/'
-      end
-
-      it "displays the login form" do
-        expect(page).to have_css("form[action='/users/sign_in']")
-      end
+    it "displays the login form" do
+      expect(page).to have_css("form[action='/users/sign_in']")
+    end
 
   end # context - when users aren't logged in
 
