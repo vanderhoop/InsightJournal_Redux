606a5db62c476ba738c5f9262803f9a133ae2ba1
Travis Vander Hoop <vanderhoop@me.com>
2013-12-28 17:17:02 -0500

    dern! left a binding.pry in one of my specs

--- spec/features/filtering_insights_spec.rb
+++ spec/features/filtering_insights_spec.rb
@@ -13,7 +13,6 @@ feature "Filtering Insights", :js => true do
       hour_mark = Entry.last.hour_created
       expect(page).to_not have_content("N/A")
       time_filter = hour_mark.between?(13, 17) ? "morning" : "afternoon"
-      binding.pry
       find("option[value=#{time_filter}]").click
       find("#filter-btn").click
       expect(page).to have_content("N/A")
