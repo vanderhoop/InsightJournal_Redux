afa20f645ad13baf523134a277058fda74481498
Travis Vander Hoop <vanderhoop@me.com>
2013-12-30 22:00:29 -0500

    added entries#index spec

--- spec/features/entry_index_page_spec.rb
+++ spec/features/entry_index_page_spec.rb
@@ -1,2 +1,33 @@
 require 'spec_helper'
 require 'selenium-webdriver'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe 'Entries Index Page', :js => true do
+  before(:each) do
+    sign_up
+  end
+
+  context "when a user has entries" do
+
+    before(:each) do
+      create_entry
+      visit '/'
+    end
+
+    # TODO write a more relevant test for the entries#index page
+
+    it "renders an archive header" do
+      click_on "All Entries"
+      expect(page).to have_content("Archive")
+    end
+
+  end # context - when a user has entries
+
+  context "when a user hasn't written an entry" do
+    # it "should tell them they shouldn't be there"
+
+  end # context - when a user hasn't written an entry
+
+end # describe - Entries Index Page
